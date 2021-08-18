377623f8770dfcf7dbe3e8aa72cc4fc723aa3e9e
Andrey Polyakov <andrey.polyakov@skillbox.ru>
2021-08-18 12:41:48 +0000

    Replace main_page.py

--- NS autotests (PageObject)/pages/main_page.py
+++ NS autotests (PageObject)/pages/main_page.py
@@ -3,6 +3,9 @@ from selenium.webdriver.common.by import By
 from pages.locators import MainPageLocators
 
 class MainPage(BasePage):
+
+    url = "https://nsstage.skillbox.ru/#/login?redirect=%2Fcampaigns%2Findex"
+
     def go_to_campaigns_page(self): # метод реализует авторизацию и переход к странице "Кампании"
         login = self.browser.find_element(*MainPageLocators.login_button)
         login.click()
